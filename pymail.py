3b3e8020b4a52d8ca9d4f57007fd652467305052
MUKTHEESWARAN MARIAPPAN <mukthy@MUKTHEESWARANs-MacBook-Pro.local>
2022-04-02 08:47:38 +0530

    Email Using Python

---
+++ pymail.py
@@ -0,0 +1,24 @@
+import smtplib
+from email.message import EmailMessage
+
+print('Login Details of your Email \n')
+username = input('Enter Your Email Address: \n')
+password = input('Enter Your Password: \n')
+
+email_address = input("Enter Email Sender's Address: \n")
+subject = input('Enter the Subject: \n')
+from_name = input('Enter the From Name: \n')
+
+email = EmailMessage()
+email['from'] = f'{from_name}'
+email['to'] = f'{email_address}'
+email['subject'] = f'{subject}'
+
+email.set_content('I am testing mail')
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login(f'{username}', f'{password}')
+    smtp.send_message(email)
+    print('Email Sent')
